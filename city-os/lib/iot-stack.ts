0f213ef505c391fd7793d8922116e0b0100dbcb5
Yuya Matsumoto <yuymt@amazon.co.jp>
2023-10-03 07:04:28 +0000

    Resolve "IoT Deviceからのデータアップロード部分作成"

--- city-os/lib/iot-stack.ts
+++ city-os/lib/iot-stack.ts
@@ -1,9 +1,10 @@
-import { CfnOutput, SecretValue, Stack, StackProps, aws_ec2, aws_rds, aws_iot, Duration } from "aws-cdk-lib";
+import { CfnOutput, SecretValue, Stack, StackProps, aws_iam, aws_iot, Duration, custom_resources } from "aws-cdk-lib";
 import * as iot_alpha from "@aws-cdk/aws-iot-alpha";
 import * as iot_actions_alpha from "@aws-cdk/aws-iot-actions-alpha";
 import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { ThingWithCert } from "cdk-iot-core-certificates";
 import { Construct } from "constructs";
 
 export interface IoTStackProps extends StackProps {}
@@ -12,15 +13,47 @@ export class IoTStack extends Stack {
   constructor(scope: Construct, id: string, props: IoTStackProps) {
     super(scope, id, props);
 
-    const rootTopicName = "data/device";
-
-    const cfnThing = new aws_iot.CfnThing(this, "Thing", {
-      thingName: "thing",
+    const iotLogPolicyStatement = new aws_iam.PolicyStatement({
+      resources: [`arn:aws:logs:${props.env!.region}:${props.env!.account}:log-group:AWSIotLogsV2:*`],
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:PutMetricFilter",
+        "logs:PutRetentionPolicy",
+        "iot:GetLoggingOptions",
+        "iot:SetLoggingOptions",
+        "iot:SetV2LoggingOptions",
+        "iot:GetV2LoggingOptions",
+        "iot:SetV2LoggingLevel",
+        "iot:ListV2LoggingLevels",
+        "iot:DeleteV2LoggingLevel",
+      ],
     });
 
-    const attatchCertificate = new aws_iot.CfnThingPrincipalAttachment(this, "ThingCertificate", {
-      principal: "",
-      thingName: "thing",
+    const iotLogPolicy = new aws_iam.Policy(this, "IotLogPolicy", {
+      statements: [iotLogPolicyStatement],
+    });
+
+    const iotLogRole = new aws_iam.Role(this, "IotLogRole", {
+      assumedBy: new aws_iam.ServicePrincipal("iot.amazonaws.com"),
+    });
+
+    iotLogRole.attachInlinePolicy(iotLogPolicy);
+
+    const cfnLogging = new aws_iot.CfnLogging(this, "MyCfnLogging", {
+      accountId: props.env!.account!,
+      defaultLogLevel: "DEBUG",
+      roleArn: iotLogRole.roleArn,
+    });
+
+    const rootTopicName = "data";
+    const sampleThingName = "sample-device-1";
+
+    const { thingArn, certId, certPem, privKey } = new ThingWithCert(this, "ThingWithCert", {
+      thingName: sampleThingName,
+      saveToParamStore: true,
+      paramPrefix: "devices",
     });
 
     const iotPolicy = new aws_iot.CfnPolicy(this, "IotPolicy", {
@@ -30,20 +63,25 @@ export class IoTStack extends Stack {
           {
             Effect: "Allow",
             Action: ["iot:Connect"],
-            Resource: [`"arn:aws:iot:${props.env!.region}:${props.env!.account}:client/*"`],
+            Resource: [`arn:aws:iot:${props.env!.region}:${props.env!.account}:client/*`],
           },
           {
             Effect: "Allow",
             Action: ["iot:Publish", "iot:Receive"],
-            Resource: [`"arn:aws:iot:${props.env!.region}:${props.env!.account}:topic/${rootTopicName}"`],
+            Resource: [`arn:aws:iot:${props.env!.region}:${props.env!.account}:topic/${rootTopicName}`],
           },
         ],
       },
       policyName: "iot-policy",
     });
 
+    const policyPrincipalAttatchment = new aws_iot.CfnPolicyPrincipalAttachment(this, "AttatchPolicy", {
+      policyName: iotPolicy.policyName!,
+      principal: `arn:aws:iot:${props.env!.region}:${props.env!.account}:cert/${certId}`,
+    });
+
     const topicRule = new iot_alpha.TopicRule(this, "TopicRule", {
-      sql: iot_alpha.IotSql.fromStringAsVer20160323(`SELECT * FROM "${rootTopicName}/*"`),
+      sql: iot_alpha.IotSql.fromStringAsVer20160323(`SELECT * FROM 'data/#'`),
     });
 
     const putDeviceDataFunction = new PythonFunction(this, "PutRecordHandler", {
@@ -52,8 +90,37 @@ export class IoTStack extends Stack {
       index: "index.py",
       memorySize: 256,
       timeout: Duration.seconds(3),
+      environment: {
+        NGSI_ENDPOINT: "",
+      },
     });
 
     topicRule.addAction(new iot_actions_alpha.LambdaFunctionAction(putDeviceDataFunction));
+
+    const getIoTEndpoint = new custom_resources.AwsCustomResource(this, "IoTEndpoint", {
+      onCreate: {
+        service: "Iot",
+        action: "describeEndpoint",
+        physicalResourceId: custom_resources.PhysicalResourceId.fromResponse("endpointAddress"),
+        parameters: {
+          endpointType: "iot:Data-ATS",
+        },
+      },
+      policy: custom_resources.AwsCustomResourcePolicy.fromSdkCalls({ resources: custom_resources.AwsCustomResourcePolicy.ANY_RESOURCE }),
+    });
+
+    const iotEndpoint = getIoTEndpoint.getResponseField("endpointAddress");
+
+    new CfnOutput(this, "IoTDevicePrivateKey", {
+      value: `https://${props.env!.region}.console.aws.amazon.com/systems-manager/parameters/`,
+    });
+
+    new CfnOutput(this, "SampleDeviceName", {
+      value: sampleThingName,
+    });
+
+    new CfnOutput(this, "IoTEndpointUrl", {
+      value: iotEndpoint,
+    });
   }
 }
